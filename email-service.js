a84fd6cdf703d1ca37df5022de497c0d88cb3ffa
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-27 20:37:02 +0500

    feat: setting path to fetch ejs file

--- email-service.js
+++ email-service.js
@@ -9,7 +9,7 @@ class EmailService {
 
     async renderEmailTemplate(templateName, data, options) {
         try {
-            const templateCompletePath = path.resolve('./') + '/' + templateName + '.ejs';
+            const templateCompletePath = path.resolve('./purchaseOrderGRN.ejs');
             return ejs.renderFile(templateCompletePath, data, options);
         } catch (err) {
             console.log('renderEmailTemplate ERRROR', err);
