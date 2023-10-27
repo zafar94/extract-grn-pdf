e1fdcc7b8cf93002102b9a80f0a7f853a76b59ef
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-27 20:29:50 +0500

    feat: created an email service for PDF template

---
+++ email-service.js
@@ -0,0 +1,20 @@
+const ejs = require('ejs');
+const path = require('path');
+
+class EmailService {
+    constructor(sesService) {
+        super();
+        this.sesService = sesService;
+    }
+
+    async renderEmailTemplate(templateName, data, options) {
+        try {
+            const templateCompletePath = path.resolve('./') + '/' + templateName + '.ejs';
+            return ejs.renderFile(templateCompletePath, data, options);
+        } catch (err) {
+            console.log('renderEmailTemplate ERRROR', err);
+        }
+    }
+}
+
+module.exports = EmailService;
